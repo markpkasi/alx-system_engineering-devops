0e61a6385274b90645fec0dce65c723c62166aa9
markpkasi <pkasimark6@gmail.com>
2023-05-03 04:49:53 -0700

    Regular exp

---
+++ 0x06-regular_expressions/5-beginning_and_end.rb
@@ -0,0 +1,5 @@
+#!/usr/bin/env ruby
+# A regular expression that matches a string that starts with h ends with n and can have any single character in between
+# the characters.
+# Ruby script that accepts one argument and pass it to a regular expression matching method.
+puts ARGV[0].scan(/h.n/).join
